1e95c242533d3d7849fc23caf1ab03edae4fec3b
Your Name <you@example.com>
2023-12-17 22:02:36 +0500

    repair

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,9 +3,16 @@ const Footer = () => {
     <footer className="site-footer bg-dark p-4 text-white">
       <div className="container">
         <div className="d-flex justify-content-center">
-          <span >
+          <span>
             Copyright &copy; {new Date().getFullYear()}
-            <a className="text-white" href="https://t.me/ShukurovShB" target="blanck"> Shukurov Shodiyor</a>
+            <a
+              className="text-white"
+              href="https://t.me/Nozila_Xudoyberdiyeva"
+              target="blanck"
+            >
+              {" "}
+              Nozila Xudoyberdiyeva
+            </a>
           </span>
         </div>
       </div>
